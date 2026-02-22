c57161e6fbfe937d0dd1a9efe604fe129a54b3ae
samrasial12-hue <samrasial12@gmail.com>
2026-02-22 10:39:54 -0800

    message

---
+++ week2/task11.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    cout<<"        ____________"<<endl;
+    cout<<" ______/| TRUCK   |\\___"<<endl;
+    cout<<"|  _ _  |_________|  _ |"<<endl;
+    cout<<"-(_)-(_)-        -(_)-(_)-"<<endl;
+}
---
+++ week2/task12.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    system("color 0B");
+
+    cout<<"        *   *   *                   "<<endl;
+    cout<<"          *   *   *                 "<<endl;
+    cout<<"            *   *   *               "<<endl;
+    cout<<"           *   *   *                "<<endl;
+    cout<<"         *    *   *                 "<<endl;
+    cout<<"          *    *    *               "<<endl;
+    cout<<"            *    *    *             "<<endl;
+    cout<<"   *****************************    "<<endl;
+    cout<<"   *                           *    "<<endl;
+    cout<<"     *                       *      "<<endl;
+    cout<<"       *                   *        "<<endl;
+    cout<<"         *               *          "<<endl;
+    cout<<"           *************            "<<endl;
+    cout << "     WELCOME TO FOODLY APPLICATION" << endl;
+}
