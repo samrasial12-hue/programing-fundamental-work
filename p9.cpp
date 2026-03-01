27326779ad18e7554fb2844ca3f6f5dacae72411
samrasial12-hue <samrasial12@gmail.com>
2026-03-01 10:33:46 -0800

    Add files via upload

---
+++ p9.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int num;
+    cout << "Enter a 4-digit number: ";
+    cin >> num;
+    int d1;
+    d1 = num % 10;
+    num = num / 10;
+    int d2;
+    d2 = num % 10;
+    num = num / 10;
+    int d3;
+    d3 = num % 10;
+    num = num / 10;
+    int d4;
+    d4 = num % 10;
+    num=num/10;
+    int sum; 
+    sum= d1 + d2 + d3 + d4;
+    cout << "Sum of digits = " << sum << endl;
+}
